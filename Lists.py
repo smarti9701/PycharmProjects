c2bb9323ff19cf2b8cb7cb9498dfa1d512d186f1
smarti9701 <stevem9701@gmail.com>
2017-09-02 14:33:16 -0400

    Working with Lists

--- Lists.py
+++ Lists.py
@@ -6,3 +6,7 @@ print(Employee2[0], Employee1[0])
 print(Employee1[0].split()[-1])  #the negative number refers to the position of the element withing the list item Steve = 0 Martinez = 1 or -1
 print(Employee1[0].split()[1])
 print(Employee2[2]*1.15)
+
+Company = [Employee1,Employee2] #creates a db with lists
+print(Company)
+
