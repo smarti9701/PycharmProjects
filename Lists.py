8114c028e8c32cc9060d71af5e87eb39f217aebd
smarti9701 <stevem9701@gmail.com>
2017-09-02 14:12:34 -0400

    Working with Lists

---
+++ Lists.py
@@ -0,0 +1,8 @@
+Employee1 = ['Steve Martinez', 44, 130000, 'Agile Coach']
+Employee2 = ['Eda Martinez', 42, 130000, 'Scrum Master']
+print(Employee1)
+print(Employee2)
+print(Employee2[0], Employee1[0])
+print(Employee1[0].split()[-1])  #the negative number refers to the position of the element withing the list item Steve = 0 Martinez = 1 or -1
+print(Employee1[0].split()[1])
+print(Employee2[2]*1.15)
