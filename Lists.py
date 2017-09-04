b908d6a39e7595f07fcebdf235d1a5e46a26614e
smarti9701 <stevem9701@gmail.com>
2017-09-04 08:25:04 -0400

    Working with Lists

--- Lists.py
+++ Lists.py
@@ -10,24 +10,24 @@ print(Employee1[0].split()[1])
 
 print(Employee2[2]*1.15)
 
-Company = [Employee1,Employee2] #creates a db with lists
-for person in Company:
+people = [Employee1, Employee2] #creates a db with lists
+for person in people:
     print(person)
 
-print(Company[0][0])  #the first reference [0] refers to the row or tuple. the second is the column in the list
-print(Company[1][0])
+print(people[0][0])  #the first reference [0] refers to the row or tuple. the second is the column in the list
+print(people[1][0])
 
 
-for person in Company:  #this will list only the first column
+for person in people:  #this will list only the first column
     print(person[0])
 
-for person in Company:  #to check pay for each person or record
+for person in people:  #to check pay for each person or record
     print(person[2])
 
-pays = [person[2] for person in Company]  #prints pay for each record
+pays = [person[2] for person in people]  #prints pay for each record
 print(pays)
 
-pays = map((lambda x:x[2]), Company)    #A use of map here would be if you start with a list
+pays = map((lambda x:x[2]), people)    #A use of map here would be if you start with a list
 # of strings instead of a single string - map can listify all of them individually
 # f = lambda x, y : x + y
 # #f(1,1)
@@ -35,12 +35,12 @@ pays = map((lambda x:x[2]), Company)    #A use of map here would be if you start
 #list(pays)  this command from the tutorial doesn't show anything because it needs the print() command
 print(list(pays))
 
-print(sum(person[2] for person in Company))
+print(sum(person[2] for person in people))
 
-Company.append(['Jazzy', 20, 10000, 'Barista'])
-print(Company)
+people.append(['Jazzy', 20, 10000, 'Barista'])
+print(people)
 print(Employee1)
 print(Employee2)
 #print(Employee3) - you can't bring up Employee3 though because it hasn't been created
-print(len(Company)) #produces the number of records
-print(Company[-1][0]) #brings up the first list item going backwards - -1 and provides the first "element" in that sublist
+print(len(people)) #produces the number of records
+print(people[-1][0]) #brings up the first list item going backwards - -1 and provides the first "element" in that sublist
