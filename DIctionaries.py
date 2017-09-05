797d61ef1613cd5a9aaf062789d02577521b6b09
smarti9701 <stevem9701@gmail.com>
2017-09-05 14:35:41 -0400

    Working with Lists

---
+++ DIctionaries.py
@@ -0,0 +1,8 @@
+steve = {'name': 'Steve Martinez', 'age': 44, 'pay': 150000, 'job': 'Agile Coach'}
+Eda = {'name': 'Eda Martinez', 'age': 42, 'pay': 150000, 'job': 'Agile Coach'}
+
+print(steve['name'], Eda['pay'])
+
+print (steve['name'].split()[-1])
+
+print(Eda['pay']*1.15)
