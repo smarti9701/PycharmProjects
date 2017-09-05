8ca477dddde16cd414c198728a5fd61198171912
smarti9701 <stevem9701@gmail.com>
2017-09-05 15:12:13 -0400

    Working with Lists

--- DIctionaries.py
+++ DIctionaries.py
@@ -1,8 +1,24 @@
-steve = {'name': 'Steve Martinez', 'age': 44, 'pay': 150000, 'job': 'Agile Coach'}
+Steve = {'name': 'Steve Martinez', 'age': 44, 'pay': 150000, 'job': 'Agile Coach'}
 Eda = {'name': 'Eda Martinez', 'age': 42, 'pay': 150000, 'job': 'Agile Coach'}
 
-print(steve['name'], Eda['pay'])
+print(Steve['name'], Eda['pay'])
 
-print (steve['name'].split()[-1])
+print (Steve['name'].split()[-1])
 
 print(Eda['pay']*1.15)
+
+
+
+
+#zipping = joining two lists together
+names = ['color', 'height', 'weight']
+values = ['black', 65, 250]
+print(list(zip(names, values)))
+
+
+
+
+Couple = [Steve, Eda]
+for person in Couple:
+    print(person['name'], person['pay'], sep=',')
+    print(person['name'], person['pay'])
