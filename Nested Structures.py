4c1dc3af9de276a62951c2a99b61ae0902e3109c
smarti9701 <stevem9701@gmail.com>
2017-09-07 16:31:40 -0400

    Working with Lists

--- Nested Structures.py
+++ Nested Structures.py
@@ -1,5 +1,10 @@
 Steve = {'name': {'first': 'Steve', 'last': 'Martinez'},
             'age': 44,
             'job': ['Agile Coach','Management Consultant'],
-            'pay': (150000, 200000)}
+            'pay': [150000, 200000]}
 
+
+print(Steve['name'])
+print(Steve['name']['last'])
+print(Steve['pay'][-1]) #this has no comma
+print(Steve['pay'],[-1]) #this one displays both the entire nest, and the additional [-1] as a separate digit because of the comma
