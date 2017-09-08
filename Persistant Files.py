6278121f50a4c19826a96a376d54169299fa7313
smarti9701 <stevem9701@gmail.com>
2017-09-08 15:54:09 -0400

    Working with Lists

---
+++ Persistant Files.py
@@ -0,0 +1,17 @@
+steve = {'name':'Steve Martinez', age=44, job='unemployed', pay=0}
+eda = {'name':'Eda Martinez', age=42, job='Scrum Master', pay=130000}
+jazzy = {'name':'Jazzy Martinez', age=20, job='student''lifetime employee', pay=10}
+amber = {'name':'Amber Martinez', age=15, job='student', pay=0}
+
+
+
+db={}
+db['steve']=steve
+db['eda']=eda
+db['jazzy']=jazzy
+db['amber']=amber
+
+#if _name_ == '_main_':
+ #   for key in db:
+  #      print(key, '=>/n', db[key])
+
