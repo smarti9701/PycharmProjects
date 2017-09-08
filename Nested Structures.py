ab3643c0784fd9c79f51aa26fde3904654d72f1e
smarti9701 <stevem9701@gmail.com>
2017-09-08 07:42:42 -0400

    Working with Lists

--- Nested Structures.py
+++ Nested Structures.py
@@ -8,3 +8,14 @@ print(Steve['name'])
 print(Steve['name']['last'])
 print(Steve['pay'][-1]) #this has no comma
 print(Steve['pay'],[-1]) #this one displays both the entire nest, and the additional [-1] as a separate digit because of the comma
+
+# almost like a SQL query
+for job in Steve['job']: #for item in Steve['job']
+    print(job)
+
+print(Steve['job'][-1])
+
+Steve['job'].append('Orgasm Donor')
+print(Steve['job'][2])
+
+print(Steve)
