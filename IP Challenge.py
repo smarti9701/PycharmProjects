a2666485eeaceaee74813c696a10694d748c0274
smarti9701 <stevem9701@gmail.com>
2017-10-06 15:48:01 -0400

    Working with Lists

--- IP Challenge.py
+++ IP Challenge.py
@@ -7,19 +7,20 @@ for char in IP:
     if char == '.':
         print("Your IP adress consists of Segment number {} containing {} Characters. ".format(segment, segmentlength,))
         segment += 1
-        segmentlength = 0 #this resets the segment length back to 0 for the next segment
+        segmentlength = 0  #this resets the segment length back to 0 for the next segment
     else:
-        segmentlength += 1 #this says that if you don't run into a '.' char, then it will increment the count of segmentlength
+        # this says that if you don't run into a '.' char, then it will increment the count of segmentlength
+        segmentlength += 1
 
-
-# this was the original way to count the last segment without a '.'
+#this was the original way to count the last segment without a '.'
 #if char != '.':
-    #print("Your IP adress consists of Segment number {} containing {} Characters. ".format(segment, segmentlength, ))
+#print("Your IP adress consists of Segment number {} containing {} Characters. ".format(segment, segmentlength, ))
 
 #This was my first attempt to account for the lack of input.
 #if char != '.' and char != '0123456789':
 
-if char != '.' and char != '0123456789' and char == "": #yes! I figured out how to just make the lack of input come out by itself.
+#yes! I figured out how to just make the lack of input come out by itself.
+if char != '.' and char != '0123456789' and char == "":
     print("Hey dummy, you forgot to enter your IP address!")
 elif char != '.':
     print("Your IP adress consists of Segment number {} containing {} Characters. ".format(segment, segmentlength, ))
