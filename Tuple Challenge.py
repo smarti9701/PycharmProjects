133544e2528dcbee96c5c2c430c40cb9e975b46d
smarti9701 <stevem9701@gmail.com>
2017-10-12 09:27:07 -0400

    Working with Lists

---
+++ Tuple Challenge.py
@@ -0,0 +1,10 @@
+imelda = "More Mayhem", "Imelda May", 2011, (( 1, "Pulling the Rug"), (2, "Pyscho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+title, artist, year, tracks = imelda
+print(imelda)
+print(title)
+print(artist)
+print(year)
+print(tracks)
+print("++"*30)
+print(title, year,)
